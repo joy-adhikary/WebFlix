7c60c7fa584e7e69cfb2a8d5e41b76d97c9827cf
joy-adhikary <joyadhikary074@gmail.com>
2023-06-23 00:55:15 +0600

    1.4/4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import Image from 'next/image'
 import './globals.css'
 import Hero from './Components/Hero'
 import Footer from './Components/footer'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 
 const Home = () => {
   return (
