cd9dc7574b15b4f7972b8687a475bc445b02bd12
joy-adhikary <joyadhikary074@gmail.com>
2023-07-15 00:23:46 +0600

    contact info updated

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div className='contact'>
+    <div className='contact '>
       {/* 3 communication from  */}
       <div className='w-screen grid grid-flow-row'>
 
@@ -64,33 +64,37 @@ export default function Contact() {
       </div>
       <div className='flex flex-col'>
         <div className='flex'>
-          <div className='text-3xl font-bold mx-auto my-10'>
+          <div className='text-3xl font-bold mx-auto my-6'>
             We&apos;d love to hear  <span className=' text-red-600'>from you</span>
           </div>
         </div>
-        <div className="flex py-10  ">
-          <div className="lg:w-1/3 md:w-1/2 bg-white rounded-2xl  drop-shadow-xl p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
-            <h2 className="text-gray-900 text-xl font-semibold text-center mb-1 font-medium title-font">Feedback</h2>
+        <div className="flex py-10">
+          <div className=" min-w-[21rem] md:min-w-[26rem] bg-white rounded-2xl  drop-shadow-xl p-8 flex flex-col mx-auto mt-10 md:mt-0 shadow-md">
+            <h2 className="text-gray-900 text-xl font-bold text-center mb-1 title-font">Feedback</h2>
             {/* <p className="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p> */}
             <div className="relative mb-4">
               <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
               <input type="email" id="email" name="email" className="w-full bg-white rounded-xl border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
             </div>
             <div className="relative mb-4">
+              <label htmlFor="email" className="leading-7 text-sm text-gray-600">Phone</label>
+              <input type="email" id="email" name="email" className="w-full bg-white rounded-xl border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            </div>
+            <div className="relative mb-4">
               <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
               <textarea id="message" name="message" className="w-full bg-white rounded-xl border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
             </div>
-            <button className="text-white w-36 text-center rounded-full bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+            <button className="text-white w-36 text-center mx-auto rounded-full bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 text-lg">Button</button>
             <p className="text-xs text-gray-500 mt-3">All right revserd by joy</p>
           </div>
         </div>
 
-        <div className="text-gray-600 body-font">
+        <div className="text-gray-600 body-font rounded-md">
           <div className=" bg-gray-300 h-96">
             <iframe width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="map" scrolling="no" src="https://maps.google.com/maps?width=100%&height=200&hl=en&q=%C4%B0joy+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed" style={{}}></iframe>
           </div>
         </div>
-        
+
       </div>
     </div>
   )
